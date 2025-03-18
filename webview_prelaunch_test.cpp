53ad96aa3cbba9fc68f751736a9de56da3df8142
Bo Cupp <pcupp@microsoft.com>
2025-03-17 20:28:30 -0700

    Remove failing test

--- webview_prelaunch_test.cpp
+++ webview_prelaunch_test.cpp
@@ -23,27 +23,6 @@ TEST(PreLaunchTest, CacheAndReadWebViewCreationArguments) {
     EXPECT_EQ(read_args->additional_browser_arguments, args.additional_browser_arguments);
 }
 
-TEST(PreLaunchTest, CacheNullBrowserExePath) {
-    // Setup test data
-    WebViewCreationArguments args;
-    args.browser_exe_path = nullptr;
-    args.user_data_dir = L"C:\\test\\user_data";
-    args.additional_browser_arguments = L"--test-arg";
-    
-    // Cache to stringstream
-    std::stringstream stream;
-    WebViewPreLaunch::CacheWebViewCreationArguments(stream, args);
-
-    // Read back from same stream
-    auto read_args = WebViewPreLaunch::ReadCachedWebViewCreationArguments(stream);
-    ASSERT_TRUE(read_args.has_value());
-
-    // Verify data matches
-    EXPECT_EQ(read_args->browser_exe_path, args.browser_exe_path);
-    EXPECT_EQ(read_args->user_data_dir, args.user_data_dir);
-    EXPECT_EQ(read_args->additional_browser_arguments, args.additional_browser_arguments);
-}
-
 TEST(PreLaunchTest, ReadInvalidJson) {
     // Test with invalid JSON data
     std::stringstream stream;
